fbdf574ca137e807815a1ba646f3fa4c32884088
nguyenhuy <huynnth2012006@fpt.edu.vn>
2021-06-22 17:17:41 +0700

    lab2

---
+++ src/MainEx2.java
@@ -0,0 +1,11 @@
+import lad2BT.ex2.Temperature;
+
+import java.util.Scanner;
+
+public class MainEx2 {
+    public static void main(String[] args) {
+        Temperature temperature = new Temperature();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
